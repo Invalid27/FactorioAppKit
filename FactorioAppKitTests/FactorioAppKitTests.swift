6c99b211abe977bf0959adb31eabcd38d5f1c42e
Magic <Roger@rex42.com>
2025-08-20 07:09:19 -0500

    Initial Commit

---
+++ FactorioAppKitTests/FactorioAppKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FactorioAppKitTests.swift
+//  FactorioAppKitTests
+//
+//  Created by Daniel on 8/20/25.
+//
+
+import Testing
+@testable import FactorioAppKit
+
+struct FactorioAppKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
